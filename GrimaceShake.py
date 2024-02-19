ddbbe56c0c1d93a599c6411fa496afcbe6c83d0c
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-02-19 13:23:14 -0600

    Tested Gas variables

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -16,6 +16,13 @@ def listOfGasStations():
     gasStationsNearby = random.choice(gasStations)
     return gasStationsNearby
 
+#function will call the gaslevelgauge to derermin out gas level and then find a close gastion
+# if we are on Low or Quarter Tank
+def gasLevelAlert():
+    milesToGasStationsLow = random.uniform(1,25)
+    milesToGasStationsQuarterTank = random.uniform(25.1,50)
+    #gasLevelGauge = gasLevelGauge()
+    print(milesToGasStationsLow)
+    print(milesToGasStationsQuarterTank)
 
-print(gasLevelGauge())
-print(listOfGasStations())
+gasLevelAlert()
